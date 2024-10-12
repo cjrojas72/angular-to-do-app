f7a8389c810e2ef8021c710a92dbf05100057e17
cjrojas72 <cjrojas72@gmail.com>
2024-10-11 20:57:09 -0400

    save work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,10 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'angular-to-do-app';
   taskList: Task[] = [];
+  tagsList: string[] = ['Work', 'Health', 'Fitness'];
+
+  isShowAllTags: boolean = true;
+  filterType: string = 'all';
 
   taskObj: Task;
   jsonData: any;
@@ -41,10 +45,36 @@ export class AppComponent {
     localStorage.setItem('ToDoApp', uTaskList);
   }
 
+  onTaskComplete(){
+    //debugger;
+    const updatedTaskList = this.taskList;
+    
+    localStorage.setItem('ToDoApp', JSON.stringify(updatedTaskList));
+  }
+
+  onRemove(index: number){
+    //debugger
+    const updatedTaskList = this.taskList.splice(index, 1);
+    localStorage.setItem('ToDoApp', JSON.stringify(updatedTaskList));
+  }
+
+  getCommaSeperatedValues(value: string): string[]{
+    const tags = value.split(',');
+    return tags
+  }
+
+  toggleShowAllTags(){
+    this.isShowAllTags = !this.isShowAllTags;
+  }
+
+  setFilter(value: string){
+    this.filterType = value;
+  }
+
 
   ngOnInit(){
     this.taskService.getTasks().subscribe((data: any) => {
-      this.jsonData = data;
+      this.jsonData = data; 
       console.log(this.jsonData);
     })
   }
@@ -54,10 +84,12 @@ export class Task{
   taskName: string;
   dueDate: string;
   tags: string;
+  isCompleted: boolean;
 
   constructor(){
     this.taskName = '';
     this.dueDate = '';
     this.tags = '';
+    this.isCompleted = false;
   }
 }
